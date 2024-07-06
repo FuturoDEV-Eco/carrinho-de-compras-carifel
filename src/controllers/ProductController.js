b8b6ebdd0ef73ea026c8c433c57bca51a4b9ddf6
Carina Felippus <carifel@gmail.com>
2024-07-06 16:02:03 -0300

    Adiciona Listar Todos os produtos

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -31,6 +31,31 @@ class ProductController extends Database{
             }catch(error){
                 response.status(500).json({ mensagem: "Não foi possível cadastrar o produto!"})
             }
-        }
     }
+        async listarTodos(request,response){
+            try{
+                const query = request.query
+                    if(query.filtro){
+                        const produtos = await this.conexao.query(`
+                            select * from products where 
+                            name ilike $1 or 
+                            amount ilike $1 or
+                            color ilike $1 or
+                            voltage ilike $1 or
+                            cast(category_id as varchar) ilike $1 or
+                            cast(price as varchar) ilike $1
+                            `, [`%S{query.filtro}%`])    
+
+                            response.json(produtos.rows)
+                    }else{
+                        const produtos = await this.conexao.query("select * from products")
+
+                        response.json(produtos.rows)
+                    }
+                   
+            }catch (error){
+                response.status(500).json({ mensagem: 'Não foi possível listar os produtos!'})
+            }
+        }
+}
         module.exports = new ProductController()
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,5 +4,6 @@ const ProductController = require('../controllers/ProductController')
 const productsRoutes = new Router()
 
 productsRoutes.post('/', ProductController.cadastrar.bind(ProductController))
+productsRoutes.get('/', ProductController.listarTodos.bind(ProductController))
 
 module.exports = productsRoutes
