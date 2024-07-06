7f58a180f0030d76a158d587d09693742b7ae1d4
Carina Felippus <carifel@gmail.com>
2024-07-05 23:07:57 -0300

    Adiciona clientes cadastro

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 const express = require('express')
+const clientsRoutes = require('./routes/clients.routes')
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.use('/clients', clientsRoutes)
+
+app.listen(port,() => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+})
